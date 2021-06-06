2fabd36ae21aa74fe44f43b970be85eff0617e19
Chiharu YASUHARA <harugcoo@me.com>
2021-06-06 18:14:24 +0900

    Initial Commit

---
+++ JingiRader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JingiRader
+//
+//  Created by Chiharu Yasuhara on R 3/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JingiRader/JingiRaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JingiRaderApp.swift
+//  JingiRader
+//
+//  Created by Chiharu Yasuhara on R 3/06/06.
+//
+
+import SwiftUI
+
+@main
+struct JingiRaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
